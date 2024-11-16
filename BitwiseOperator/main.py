114eb72161a030cd52ee84ce4365909cf2ac8e7d
Trekker687 <113757673+Trekker687@users.noreply.github.com>
2024-11-16 17:44:23 +0530

    Add files via upload

---
+++ BitwiseOperator/main.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a positive number"))
+b = int(input("Enter a negative number"))
+
+#print bitwise right shift operator
+print("a >> 1=", a>>1)
+print("b >> 1 =", b>>1)
+
+
+#print bitwise left shift operator
+print("a << 1=", a<<1)
+print("b << 1 =", b<<1)
